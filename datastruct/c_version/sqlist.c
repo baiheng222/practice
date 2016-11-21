70bf320a97d448caf4cc533bb0c5adfd2140cce0
baiheng222 <baiheng222@yahoo.com>
2016-11-21 21:36:54 +0800

    fix bugs

--- datastruct/c_version/sqlist.c
+++ datastruct/c_version/sqlist.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <time.h>
 
 #define TRUE	1
 #define FALSE	0
 #define OK		1
 #define ERROR	0
 #define OVERFLOW	-2
-#define LIST_INIT_SIZE	10
-#define	LISTINCREMENT	10
+#define LIST_INIT_SIZE	20
+#define	LISTINCREMENT	20
 
 typedef int Status;
 typedef int ElemType;
@@ -20,6 +21,13 @@ typedef struct
 	int length;
 	int listsize;
 }Sqlist;
+
+//生成num以内的随机数
+int getRand(int num)
+{
+    srand((unsigned)time(NULL)); //用于保证是随机数
+    return rand()%num;  //用rand产生随机数并设定范围
+}
 
 Status InitList_Sq(Sqlist *L)
 {
@@ -49,6 +57,7 @@ void DestoryList(Sqlist *L)
 	
 	if (L->elem != NULL)
 	{
+        printf("\tfree elem\n");
 		free(L->elem);
 		L->elem == NULL;
 		L->length = 0;
@@ -74,19 +83,19 @@ void ClearList(Sqlist *L)
 
 Status ListEmpty(Sqlist *L)
 {
-	if (NULL != L)
+    if (NULL == L)
 	{
-		printf("NULL list!\n");
+        printf("\tNULL list!\n");
 		return ERROR;
 	}	
 	
-	if (L->length < 1)
+    if (L->length == 0)
 	{
-		return ERROR;
+        return TRUE;
 	}			
 	else
 	{
-		return OK;
+        return FALSE;
 	}	
 }
 
@@ -198,6 +207,7 @@ Status ListInsert(Sqlist *L, int i, ElemType e)
 
 	if (i < 1 || i > L->length+1)
 	{
+        printf("\ti > length+1 \n");
 		return ERROR;
 	}
 
@@ -217,14 +227,14 @@ Status ListInsert(Sqlist *L, int i, ElemType e)
 
 	ElemType *q = &(L->elem[i-1]);
 	ElemType *p;
-	for (p = &(L->elem[L->length -1]); p >= q; p--)
+    for (p = &(L->elem[L->length -1]); p >= q; --p)
 	{
 		*(p+1) = *p;
 	}
 
 	*q = e;
 	
-	(L->length)++;
+    ++(L->length);
 
 	return OK;
 	
@@ -256,12 +266,146 @@ Status ListDelete(Sqlist *L, int i, ElemType *e)
 	return OK;
 }
 
+Status ListTraverse(Sqlist *L)
+{
+	if (NULL == L)
+	{
+		return ERROR;
+	}
+
+    if (L->length == 0)
+    {
+        printf("Empty list\n");
+        return ERROR;
+    }
+    int i = 1;
+    for (i = 1; i <= L->length; i++)
+    {
+        printf("\tlist[%d] is %d \n", i, L->elem[i]);
+    }
+
+    return OK;
+
+}
+
+void ListSort(Sqlist *L)
+{
+    if (NULL == L)
+    {
+        return;
+    }
+
+    int i, j, max, tmp;
+    for (i = 0; i < L->length - 1; i++)
+    {
+        max = i;
+        for (j = i; j < L->length; j++)
+        {
+            if (L->elem[max] < L->elem[j])
+            {
+                max = j;
+            }
+        }
+
+        tmp = L->elem[i];
+        L->elem[i] = L->elem[max];
+        L->elem[max] = tmp;
+    }
+}
+
 int main(int argc, char **argv)
 {
-	Sqlist list;
-	Status ret = InitList_Sq(&list);
-	
-	DestoryList(&list);
+    int i;
+    ElemType e;
+    Sqlist L;
+
+    srand((int) time(0));
+
+    Status ret = InitList_Sq(&L);
+    if (ret == ERROR)
+    {
+        printf("\tInitList error\n");
+        return -1;
+    }
+    else
+    {
+        printf("\tInitlist success\n");
+    }
+
+
+    /*判断该线性表是否为空*/
+    printf("\n\t列表是否为空？\n");
+    if(ListEmpty(&L) == TRUE)
+    {
+        printf("\t该线性表是为空！\n\n");
+    }
+    else
+    {
+        printf("\t该线性表不为空！\n\n");
+    }
+
+
+    /*该线性表的长度*/
+    printf("\n\t该线性表的长度：%d\n\n", ListLength(&L));
+
+
+    /*向线性表中插入元素*/
+    printf("\n\t插入元素\n");
+    for(i = 1; i <= 8; i++)
+    {
+        ListInsert(&L, i, rand()%100);
+    }
+
+    ListTraverse(&L);
+    printf("\n\t该线性表的长度：%d\n\n", ListLength(&L));
+
+
+    /*查找数的前驱*/
+    printf("\n\t将要查找元素的前驱！\n");
+    printf("\t输入要查找的值: ");
+    scanf("%d", &i);
+    if(PriorElem(&L, i, &e) == 1)
+    {
+        printf("\t存在前驱，且前驱为：%d\n\n", e);
+    }
+    else
+    {
+        printf("\t不存在前驱元素！\n\n");
+    }
+
+    /*查找数的后继*/
+    printf("\n\t将要查找元素的后继！\n");
+    printf("\t输入要查找的值: ");
+    scanf("%d", &i);
+    if(NextElem(&L, i, &e) == 1)
+    {
+        printf("\t存在后继，且后继为：%d\n\n", e);
+    }
+    else
+    {
+        printf("\t不存在后继元素！\n\n");
+    }
+
+    /*删除线性表中的第i个元素*/
+    printf("\n\t删除元素！\n");
+    printf("\n\t删除前：\n");
+    ListTraverse(&L);
+    printf("\t输入要删除的第几个元素：");
+    scanf("%d", &i);
+    ListDelete(&L, i, &e);
+    printf("\n\t删除后：\n");
+    ListTraverse(&L);
+
+
+    /*元素排序*/
+    printf("\n\t排序前：\n");
+    ListTraverse(&L);
+    printf("\n\t排序后：\n");
+    ListSort(&L);
+    ListTraverse(&L);
+
+
+    DestoryList(&L);
 	
 	return ret;
 }
