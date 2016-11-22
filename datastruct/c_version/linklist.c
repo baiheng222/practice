e9cb9a02880a1082aad0cc95614fd65166567726
baiheng222 <baiheng222@yahoo.com>
2016-11-22 18:02:03 +0800

    add linklist.c

---
+++ datastruct/c_version/linklist.c
@@ -0,0 +1,27 @@
+/*
+ *链表的实现
+ */
+
+#include <stdio.h>
+
+#define TRUE    1
+#define FALSE   0
+#define OK      1
+#define ERROR   0
+#define OVERFLOW    -2
+#define LIST_INIT_SIZE  20
+#define LISTINCREMENT   20
+
+typedef int Status;
+
+typedef int ElemType;
+struct Node
+{
+	ElemType elem;
+	Node *next;
+}
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
